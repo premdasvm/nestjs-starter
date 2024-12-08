68632d81d52228046893db06ba720759e3349586
Prem <premdasvm8@gmail.com>
2024-12-08 23:05:13 +0530

    feat(init): Bootstrap NestJS project
    - Initialize core project structure
    - Configure Prettier with custom formatting rules
    - Apply consistent code formatting across the entire repository

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+export default {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'always',
+};
